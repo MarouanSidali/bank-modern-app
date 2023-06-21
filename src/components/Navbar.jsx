7ab304a2a475ec51d6127dd6518d33120adb29e5
MarouanSidali <marwandx57@gmail.com>
2023-06-21 14:14:42 +0000

    Started working  on the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,38 @@
-import { useState } from "react"
-import {close , menu , logo } from '../assets'
-import { navLinks } from '../constansts'
+import { useState } from "react";
+import { close, menu, logo } from "../assets";
+import { navLinks } from "../constansts";
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
-      <img src={logo} alt="hookBank" />
-    </nav>)
-  
-}
+      <img src={logo} alt="hookBank" className="w-[124px] h-[32px]" />
+      <ul className="list-none sm:flex hidden flex-1 justify-end items-center">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${
+              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+            } text-white`}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+      <div className="flex flex-1 items-center sm:hidden justify-end">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] height-[28px] object-contain"
+          onClick={() => setToggle((prev) => !prev)}
+        />
+      </div>
+      <div
+        className={`${
+          toggle ? "flex" : "hidden"
+        } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+      ></div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
