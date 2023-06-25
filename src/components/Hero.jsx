0eb98b5b30d04e050acfac8cf0ad303089d3a554
MarouanSidali <marwandx57@gmail.com>
2023-06-25 08:09:17 +0000

    Finished the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -38,9 +38,12 @@ const Hero = () => {
       </div>
       <div>
         <img src={robot} alt="Robot" className="w-[100%] h-[100%] relative z-[5] " />
+        {/*
+            ******Gradient On the hero section image*******
+
         <div className="absolute z-0 w-[40%] h-[35%] top-0 pink__gradient" />
         <div className="absolute z-1 w-[80%] h-[80%] rounded-full bottom-40 white__gradient" />
-        <div className="absolute z-0 w-[50%] h-[50%] bottom-20 blue__gradient" />
+        <div className="absolute z-0 w-[50%] h-[50%] bottom-20 blue__gradient" />*/}
       </div>
     </section>
   );
